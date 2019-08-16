a43fe8f2b042aaf986074cd8dc47754a8b2b2d84
Andrew August <aaugust288@gmail.com>
2019-08-15 22:27:45 -0700

    commit

--- make_songbook.py
+++ make_songbook.py
@@ -11,20 +11,20 @@ The tabs in tabs/ need to be named in the form <artist>-<songname>.
 The output html is songbook.html.
 '''
 
-tab_files = sorted(glob('tabs/*.txt'))
+tab_files = sorted(glob('tabs_raw/*.txt'))
 
+# Make table of links
 table_str = ''
-
 for tab_file in tab_files:
-    artist, song = osp.basename(tab_file).split('-')
-    song = song.split('.')[0]
-    artist = artist.replace('_',' ')
-    song = song.replace('_',' ')
+    artist, song_name = osp.basename(tab_file).split('-')
+    song_name = song_name.split('.')[0]
+    song_name = song_name.replace('_',' ')
+    artist = artist.replace('_', ' ')
 
     row = f'''
     <tr>
     <td>{artist}</td>
-    <td><a href="{tab_file}">{song}</a></td>
+    <td><a href="{tab_file}">{song_name}</a></td>
     </tr>'''
 
     table_str += row
@@ -37,3 +37,27 @@ html_str = html_str.replace('#TABLEROWS#', table_str)
 
 with open('songbook.html','w') as f:
     f.write(html_str)
+
+
+# Make html pages
+with open('song_template.html', 'r') as f:
+    tab_template_str = f.read()
+
+for tab_file in tab_files:
+    artist, song_name = osp.basename(tab_file).split('-')
+    song_name = song_name.split('.')[0]
+    song_name = song_name.replace('_',' ')
+    artist = artist.replace('_', ' ')
+
+    with open(tab_file, 'r') as f:
+        tab = f.read()
+
+    tab = tab.replace('\n','<br>')
+
+    tab_str = tab_template_str.replace('#TITLE#', f'{artist} - {song_name}')
+    tab_str = tab_str.replace('#ARTIST_NAME#', artist)
+    tab_str = tab_str.replace('#SONG_NAME#', song_name)
+    tab_str = tab_str.replace('#TAB#', tab)
+
+    with open(f'tabs_html/{osp.basename(tab_file).split(".")[0]}.html','w') as f:
+        f.write(tab_str)
